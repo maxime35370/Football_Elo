1ad39078382aa7630b51f5bba77d5ddac6307d4f
Maxime <maxime.theard@gmail.com>
2025-09-19 17:58:35 +0200

    Ajout des équipes de test avec structure Elo et stats traditionnelles

---
+++ data/teams.js
@@ -0,0 +1,146 @@
+{
+  "teams"; [
+    {
+      "id": 1,
+      "name": "Paris Saint-Germain",
+      "shortName": "PSG",
+      "city": "Paris",
+      "eloRating": 1500,
+      "traditionalStats": {
+        "points": 0,
+        "played": 0,
+        "won": 0,
+        "drawn": 0,
+        "lost": 0,
+        "goalsFor": 0,
+        "goalsAgainst": 0,
+        "goalDifference": 0
+      }
+    },
+    {
+      "id": 2,
+      "name": "Olympique de Marseille",
+      "shortName": "OM",
+      "city": "Marseille",
+      "eloRating": 1500,
+      "traditionalStats": {
+        "points": 0,
+        "played": 0,
+        "won": 0,
+        "drawn": 0,
+        "lost": 0,
+        "goalsFor": 0,
+        "goalsAgainst": 0,
+        "goalDifference": 0
+      }
+    },
+    {
+      "id": 3,
+      "name": "Olympique Lyonnais",
+      "shortName": "OL",
+      "city": "Lyon",
+      "eloRating": 1500,
+      "traditionalStats": {
+        "points": 0,
+        "played": 0,
+        "won": 0,
+        "drawn": 0,
+        "lost": 0,
+        "goalsFor": 0,
+        "goalsAgainst": 0,
+        "goalDifference": 0
+      }
+    },
+    {
+      "id": 4,
+      "name": "AS Monaco",
+      "shortName": "ASM",
+      "city": "Monaco",
+      "eloRating": 1500,
+      "traditionalStats": {
+        "points": 0,
+        "played": 0,
+        "won": 0,
+        "drawn": 0,
+        "lost": 0,
+        "goalsFor": 0,
+        "goalsAgainst": 0,
+        "goalDifference": 0
+      }
+    },
+    {
+      "id": 5,
+      "name": "Lille OSC",
+      "shortName": "LOSC",
+      "city": "Lille",
+      "eloRating": 1500,
+      "traditionalStats": {
+        "points": 0,
+        "played": 0,
+        "won": 0,
+        "drawn": 0,
+        "lost": 0,
+        "goalsFor": 0,
+        "goalsAgainst": 0,
+        "goalDifference": 0
+      }
+    },
+    {
+      "id": 6,
+      "name": "Stade Rennais",
+      "shortName": "SRFC",
+      "city": "Rennes",
+      "eloRating": 1500,
+      "traditionalStats": {
+        "points": 0,
+        "played": 0,
+        "won": 0,
+        "drawn": 0,
+        "lost": 0,
+        "goalsFor": 0,
+        "goalsAgainst": 0,
+        "goalDifference": 0
+      }
+    },
+    {
+      "id": 7,
+      "name": "OGC Nice",
+      "shortName": "OGCN",
+      "city": "Nice",
+      "eloRating": 1500,
+      "traditionalStats": {
+        "points": 0,
+        "played": 0,
+        "won": 0,
+        "drawn": 0,
+        "lost": 0,
+        "goalsFor": 0,
+        "goalsAgainst": 0,
+        "goalDifference": 0
+      }
+    },
+    {
+      "id": 8,
+      "name": "RC Strasbourg",
+      "shortName": "RCS",
+      "city": "Strasbourg",
+      "eloRating": 1500,
+      "traditionalStats": {
+        "points": 0,
+        "played": 0,
+        "won": 0,
+        "drawn": 0,
+        "lost": 0,
+        "goalsFor": 0,
+        "goalsAgainst": 0,
+        "goalDifference": 0
+      }
+    }
+  ],
+  "matches"; [],
+  "settings"; {
+    "eloKFactor"; 32,
+    "initialEloRating"; 1500,
+    "season"; "2025-2026"
+  }
+}
